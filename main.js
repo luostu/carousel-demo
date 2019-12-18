e765a2ac8fea568194a606ee6e022ce23e7f8c9f
luostu <935663182@qq.com>
2019-12-18 20:54:08 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+
+var allButtons=$("#buttons>ul>li")
+for(let i=0;i<allButtons.length;i++){
+  $(allButtons[i]).on('click',
+  function(x){
+    var index=$(x.currentTarget).index()
+    var p=index * -920
+    $('#images').css({
+      transform:'translate('+p+'px)'
+    })
+    let n=index
+    activeButton(allButtons.eq(n))
+  }
+  )
+}
+function activeButton($button){
+  $button
+  .addClass('color')
+ .siblings('.color') .removeClass('color')
+  
+}
+var n=0
+var size=allButtons.length
+allButtons.eq(n % size).trigger('click')
+
+var timerId=setTimer()
+function setTimer(){
+  return setInterval(()=>
+{
+   n+=1
+  allButtons.eq(n % size).trigger('click')
+
+},5000)
+}
+
+$('.window').on('mouseenter',function(){
+  window.clearInterval(timerId)
+})
+$('.window').on('mouseleave',function(){
+ timerId=setTimer()
+  })
+  
+  
+
+
+
